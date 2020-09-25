e809aa0a4ba9581db9b55f0975722d2e84bec057
Ellpeck <me@ellpeck.de>
2020-09-25 23:05:47 +0200

    added the option to add a custom size to meteors in the spawn command

--- src/main/java/de/ellpeck/nyx/commands/CommandMeteor.java
+++ src/main/java/de/ellpeck/nyx/commands/CommandMeteor.java
@@ -7,6 +7,8 @@ import net.minecraft.command.*;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 
+import java.util.Optional;
+
 public class CommandMeteor extends CommandBase {
     @Override
     public String getName() {
@@ -20,18 +22,24 @@ public class CommandMeteor extends CommandBase {
 
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-        if (args.length != 0 && args.length != 2)
+        if (args.length != 0 && args.length != 2 && args.length != 3)
             throw new WrongUsageException(this.getUsage(sender));
 
         double x = sender.getPosition().getX();
         double z = sender.getPosition().getZ();
-        if (args.length == 2) {
+        Integer size = null;
+        if (args.length >= 2) {
             x = parseDouble(x, args[0], false);
             z = parseDouble(z, args[1], false);
+            if (args.length == 3)
+                size = parseInt(args[2], 1);
         }
 
         BlockPos pos = new BlockPos(x, 0, z);
-        pos = FallingMeteor.spawn(sender.getEntityWorld(), pos).getPosition();
+        FallingMeteor meteor = FallingMeteor.spawn(sender.getEntityWorld(), pos);
+        if (size != null)
+            meteor.size = size;
+        pos = meteor.getPosition();
         notifyCommandListener(sender, this, "command.nyx.meteor.success", pos.getX(), pos.getY(), pos.getZ());
     }
 }
