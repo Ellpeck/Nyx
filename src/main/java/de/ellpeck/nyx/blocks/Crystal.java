9d403ceabd82928b48a3fea2f73808cb13a1329b
Ellpeck <me@ellpeck.de>
2020-09-30 18:45:37 +0200

    made the crystal be new now

--- src/main/java/de/ellpeck/nyx/blocks/Crystal.java
+++ src/main/java/de/ellpeck/nyx/blocks/Crystal.java
@@ -3,6 +3,8 @@ package de.ellpeck.nyx.blocks;
 import de.ellpeck.nyx.Config;
 import de.ellpeck.nyx.Nyx;
 import de.ellpeck.nyx.Registry;
+import de.ellpeck.nyx.capabilities.NyxWorld;
+import de.ellpeck.nyx.lunarevents.HarvestMoon;
 import net.minecraft.block.*;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
@@ -34,12 +36,18 @@ public class Crystal extends BlockContainer {
         this.setHardness(3);
         this.setLightLevel(0.9375F);
         this.setTickRandomly(true);
-        Registry.initBlock(this, "crystal", Item::new);
+        Registry.initBlock(this, "crystal", ItemBlock::new);
         GameRegistry.registerTileEntity(Tile.class, this.getRegistryName());
     }
 
     @Override
     public void randomTick(World worldIn, BlockPos pos, IBlockState s, Random random) {
+        TileEntity tile = worldIn.getTileEntity(pos);
+        if (!(tile instanceof Tile))
+            return;
+        Tile crystal = (Tile) tile;
+        if (crystal.durability <= 0)
+            return;
         int range = 5;
         for (int x = -range; x <= range; x++) {
             for (int z = -range; z <= range; z++) {
@@ -50,6 +58,11 @@ public class Crystal extends BlockContainer {
                     if (block instanceof IGrowable || block instanceof IPlantable) {
                         for (int i = 0; i < 5; i++)
                             block.randomTick(worldIn, offset, state, random);
+                        if (worldIn.getBlockState(offset) != state) {
+                            crystal.durability--;
+                            if (crystal.durability <= 0)
+                                return;
+                        }
                     }
                 }
             }
@@ -87,33 +100,6 @@ public class Crystal extends BlockContainer {
     }
 
     @Override
-    public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
-        ItemStack drop = new ItemStack(this, 1);
-        TileEntity tile = world.getTileEntity(pos);
-        if (tile instanceof Tile)
-            drop.setItemDamage(((Tile) tile).durability);
-        drops.add(drop);
-    }
-
-    @Override
-    public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
-        TileEntity tile = worldIn.getTileEntity(pos);
-        if (tile instanceof Tile)
-            ((Tile) tile).durability = stack.getItemDamage();
-    }
-
-    @Override
-    public boolean removedByPlayer(IBlockState state, World world, BlockPos pos, EntityPlayer player, boolean willHarvest) {
-        return willHarvest || super.removedByPlayer(state, world, pos, player, false);
-    }
-
-    @Override
-    public void harvestBlock(World worldIn, EntityPlayer player, BlockPos pos, IBlockState state, @Nullable TileEntity te, ItemStack stack) {
-        super.harvestBlock(worldIn, player, pos, state, te, stack);
-        worldIn.setBlockToAir(pos);
-    }
-
-    @Override
     public TileEntity createNewTileEntity(World worldIn, int meta) {
         return new Tile();
     }
@@ -123,35 +109,7 @@ public class Crystal extends BlockContainer {
         return EnumBlockRenderType.MODEL;
     }
 
-    public static class Item extends ItemBlock {
-
-        public Item(Block block) {
-            super(block);
-            this.setMaxDamage(Config.crystalDurability);
-        }
-
-        @Override
-        public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-            if (!player.isSneaking()) {
-                IBlockState state = worldIn.getBlockState(pos);
-                Block block = state.getBlock();
-                if (block instanceof IGrowable) {
-                    IGrowable growable = (IGrowable) block;
-                    if (growable.canGrow(worldIn, pos, state, worldIn.isRemote)) {
-                        if (!worldIn.isRemote && growable.canUseBonemeal(worldIn, worldIn.rand, pos, state)) {
-                            growable.grow(worldIn, worldIn.rand, pos, state);
-                            worldIn.playEvent(2005, pos, 0);
-                            player.getHeldItem(hand).damageItem(1, player);
-                        }
-                    }
-                    return EnumActionResult.SUCCESS;
-                }
-            }
-            return super.onItemUse(player, worldIn, pos, hand, facing, hitX, hitY, hitZ);
-        }
-    }
-
-    public static class Tile extends TileEntity {
+    public static class Tile extends TileEntity implements ITickable {
 
         public int durability;
 
@@ -165,6 +123,15 @@ public class Crystal extends BlockContainer {
         public void readFromNBT(NBTTagCompound compound) {
             this.durability = compound.getInteger("durability");
             super.readFromNBT(compound);
+        }
+
+        @Override
+        public void update() {
+            if (!this.world.isRemote && this.world.getTotalWorldTime() % 600 == 0) {
+                NyxWorld data = NyxWorld.get(this.world);
+                if (data != null && data.currentEvent instanceof HarvestMoon && this.world.canSeeSky(this.pos.up()))
+                    this.durability = Math.min(Config.crystalDurability, this.durability + Config.crystalDurability / 10);
+            }
         }
     }
 }
