ad10df7af70e86bc2ceac04ebf28757839270af7
Ellpeck <me@ellpeck.de>
2020-10-21 16:56:37 +0200

    meteor walking inflicts hot floor damage

--- src/main/java/de/ellpeck/nyx/blocks/MeteorRock.java
+++ src/main/java/de/ellpeck/nyx/blocks/MeteorRock.java
@@ -6,8 +6,12 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -41,5 +45,12 @@ public class MeteorRock extends Block {
     @Override
     public Item getItemDropped(IBlockState state, Random rand, int fortune) {
         return this.droppedItem.get();
+    }
+
+    @Override
+    public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
+        if (!entityIn.isImmuneToFire() && entityIn instanceof EntityLivingBase && !EnchantmentHelper.hasFrostWalkerEnchantment((EntityLivingBase) entityIn))
+            entityIn.attackEntityFrom(DamageSource.HOT_FLOOR, 1);
+        super.onEntityWalk(worldIn, pos, entityIn);
     }
 }
